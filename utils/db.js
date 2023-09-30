c49ff8c9191148bb100cfa5bd084389d90a8636b
jamesbright <jamesononiwu@gmail.com>
2023-09-30 01:03:13 +0100

    added tests

--- utils/db.js
+++ utils/db.js
@@ -6,31 +6,32 @@ const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
-    constructor() {
-        MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
-            if (!error) {
-                this.db = client.db(DB_DATABASE);
-                this.users = this.db.collection('users');
-                this.files = this.db.collection('files');
-            } else {
-                console.log(error.message);
-                this.db = false;
-            }
-        });
-    }
+  constructor() {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      if (!error) {
+        this.db = client.db(DB_DATABASE);
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      } else {
+        console.log(error.message);
+        this.db = false;
+      }
+    });
+  }
 
-    isAlive() {
-        return !!this.db;
-    }
-    async nbUsers() {
-        const userCount = this.users.countDocuments();
-        return userCount;
-    }
+  isAlive() {
+    return !!this.db;
+  }
 
-    async nbFiles() {
-        const fileCount = this.files.countDocuments();
-        return fileCount;
-    }
+  async nbUsers() {
+    const userCount = this.users.countDocuments();
+    return userCount;
+  }
+
+  async nbFiles() {
+    const fileCount = this.files.countDocuments();
+    return fileCount;
+  }
 }
 
 const dbClient = new DBClient();
--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,8 @@
 const redis = require('redis');
-const { promisify } = require('utils');
+const { promisify } = require('util');
 
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = redis.createClient();
 
     this.getAsyncFunc = promisify(this.client.get).bind(this.client);
@@ -12,19 +12,19 @@ class RedisClient {
     });
   }
 
-  isAlive () {
+  isAlive() {
     return this.client.connected;
   }
 
-  async get (key) {
+  async get(key) {
     return this.getAsyncFunc(key);
   }
 
-  async set (key, value, duration) {
+  async set(key, value, duration) {
     this.client.setex(key, duration, value);
   }
 
-  async del (key) {
+  async del(key) {
     this.client.del(key);
   }
 }
