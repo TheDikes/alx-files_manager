b30ab21bf5553a7edb9d2ad1f60e8e21481a7812
jamesbright <jamesononiwu@gmail.com>
2023-10-02 00:22:30 +0100

    implement files action

--- routes/index.js
+++ routes/index.js
@@ -29,6 +29,30 @@ function Routing(app) {
   router.get('/users/me', (req, res) => {
     UserController.getMe(req, res);
   });
+
+  router.post('/files', (req, res) => {
+    FilesController.postUpload(req, res);
+  });
+
+  router.get('/files/:id', (req, res) => {
+    FilesController.getShow(req, res);
+  });
+
+  router.get('/files', (req, res) => {
+    FilesController.getIndex(req, res);
+  });
+
+  router.put('/files/:id/publish', (req, res) => {
+    FilesController.putPublish(req, res);
+  });
+
+  router.put('/files/:id/unpublish', (req, res) => {
+    FilesController.putUnpublish(req, res);
+  });
+
+  router.get('/files/:id/data', (req, res) => {
+    FilesController.getFile(req, res);
+  });
 }
 
 export default Routing;
